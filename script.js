91b9c7a664e03b7084e45a540a77df7eeab2cda7
Sümeyye <sumeyye.aydinn07@gmail.com>
2021-12-10 22:01:12 +0300

    dog age project done

---
+++ script.js
@@ -0,0 +1,23 @@
+let myAge = prompt("Yaşınızı Girin");
+
+
+//let myAge= 29; // benim yaşım
+let earlyYears=2; // köpek yılındaki ilk yıl
+
+earlyYears *=10.5; // aynısı buna eşit earlyYears = earlyYears*10.5;
+let laterYears = myAge - 2; // kendi yaşımızdan 2 çıkardık
+laterYears = laterYears*4; //bu ifade ile laterYears *=4; eşittir. 
+console.log (laterYears);
+console.log (earlyYears);
+
+let myAgeInDogYears= earlyYears + laterYears; // toplam köpek yaşımız
+let myName= "GUL";
+
+console.log(myName.toLowerCase());
+console.log(`Benim adım ${myName}. Köpek yılı olarak ${myAgeInDogYears} yaşındayım, insan yılı olarak ${myAge} yaşındayım.`); // açıklayıcı cümle
+
+let kopekyasi = 129;
+let ilk2yil = 10.5*2;
+let yas = kopekyasi-ilk2yil;
+let sonyas = yas/4+2;
+console.log(sonyas); 
